3bee54c6e1e91eeb0a2559c937d806c720e50e71
AnujBelgaonkar <anuj.belgaonkar@gmail.com>
2024-04-09 19:39:06 +0530

    deleted enviroment

--- resources1.py
+++ resources1.py
@@ -1,6 +1,5 @@
 import numpy as np
 import tensorflow as tf
-import random
 from tensorflow import keras
 from tensorflow.keras.layers import Dense, Dropout, Input, BatchNormalization, Conv2D, MaxPooling2D, Average
 from tensorflow.keras.layers import Flatten,AveragePooling2D,AveragePooling2D,Activation,Attention,Multiply,SeparableConv2D
@@ -11,14 +10,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import activations
 from tensorflow.keras import regularizers
-import math
 import streamlit as st
-from langchain.llms import Ollama
-
-#@st.cache_resource
-def load_llm():
-    return Ollama(base_url='http://localhost:11434',
-                model="orca-mini")
 
 #@st.cache_resource
 def get_model() -> Model:
