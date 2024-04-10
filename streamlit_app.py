c7467f9f221a143b8a9a6766a2b695245491b736
AnujBelgaonkar <anuj.belgaonkar@gmail.com>
2024-04-10 11:19:23 +0530

    vsv

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ def init_session_state():
     if 'counter' not in st.session_state:
         st.session_state.counter = 1
 
-#os.environ["TOGETHER_API_KEY"] = st.secrets["TOGETHER_API_KEY"]
-os.environ["TOGETHER_API_KEY"] = os.getenv("TOGETHER_API_KEY")
+os.environ["TOGETHER_API_KEY"] = st.secrets["TOGETHER_API_KEY"]
+#os.environ["TOGETHER_API_KEY"] = os.getenv("TOGETHER_API_KEY")
 
 path = r"model.weights.h5"
 def add_data(data):
