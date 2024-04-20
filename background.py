4de2eb78b4df76bf1433cca019536085dfdfd698
AnujBelgaonkar <anuj.belgaonkar@gmail.com>
2024-04-20 09:06:10 +0530

    made pleasent

--- background.py
+++ background.py
@@ -11,7 +11,7 @@ def apply_background():
     background-attachment: local;
     }}
     [data-testid="stSidebar"] > div:first-child {{
-    background-image: url("https://images.unsplash.com/photo-1523741543316-beb7fc7023d8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+    background-image: url("https://images.unsplash.com/photo-1525947088131-b701cd0f6dc3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
     background-position: center; 
     background-repeat: no-repeat;
     background-attachment: fixed;
